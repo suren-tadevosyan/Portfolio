aa5c5e7eddf436411ace6e1c784e41e47c7955c7
suren <tadevosyansuren07@gmail.com>
2024-02-22 12:47:19 +0400

    changed profile pic

--- app/page.jsx
+++ app/page.jsx
@@ -11,20 +11,15 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col items-center lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+      <div className="h-full flex flex-col items-center lg:flex-row-reverse px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 w-full lg:h-full lg:w-1/2 relative">
-          <Image
-            src="/hero.png"
-            alt=""
-            fill
-            className="object-contain"
-          />
+        <div className="h-1/2 w-full lg:h-full lg:w-1/2 lg:mg-r-20 relative">
+          <Image src="/img.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
-          <h1 className="text-4xl md:text-6xl font-bold overflow-y-hidden">
+          <h1 className=" sm:text-2.5xl lg:text-6xl md:text-4xl font-bold overflow-y-hidden ">
             Crafting Digital Experiences, Designing Tomorrow.
           </h1>
           {/* DESC */}
